e92cf85d291ac29518fd0d91d66544bb4d6437b1
Manfred Araujo <manfred.araujo.2021@live.rhul.ac.uk>
2023-10-15 17:30:34 +0100

    Added some testing for the sudoku solvers and updated the diary
    I simply run the tester against a range of different difficulties and
    see how well the puzzles did.

--- FinalYearProject/backend/SudokuSolver2.py
+++ FinalYearProject/backend/SudokuSolver2.py
@@ -15,7 +15,7 @@ class SudokuSolver2:
         self.sudoku = grid
         self.heap = heap()
         self.removed = {}
-
+        self.setupHeap()
     
     def solver(self):
         '''
@@ -24,8 +24,6 @@ class SudokuSolver2:
         Returns
         The completed grid if a solution was found, False otherwise.
         '''
-
-        self.setupHeap()
         if self.solve(self.heap) == True:
             return self.sudoku.grid
         return False
